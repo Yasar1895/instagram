4f1611fac938f0eea0a81e4cb3078c555fa6c0a3
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-16 18:24:15 +0530

    Create Messages.jsx

---
+++ src/pages/Messages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MessagePanel from "../components/MessagePanel";
+
+export default function Messages() {
+  return (
+    <div className="container">
+      <h1>Messages</h1>
+      <MessagePanel />
+    </div>
+  );
+}
