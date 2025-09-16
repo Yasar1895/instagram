391507b4663b24003bf519607b28ddfb79e5c8eb
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-16 22:37:22 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,37 @@
+import React from "react";
 import Stories from "../components/Stories";
 import Post from "../components/Post";
 
-export default function Home() {
+const Home = () => {
+  const posts = [
+    {
+      id: 1,
+      username: "alex",
+      userImg: "/images/story2.jpg",
+      image: "/images/post1.jpg",
+      caption: "Beautiful sunset 🌅",
+      likes: 120,
+      comments: 15,
+    },
+    {
+      id: 2,
+      username: "maria",
+      userImg: "/images/story3.jpg",
+      image: "/images/post2.jpg",
+      caption: "Chilling with friends 🍕",
+      likes: 200,
+      comments: 40,
+    },
+  ];
+
   return (
-    <div className="page">
+    <div className="home">
       <Stories />
-      <Post
-        username="john_doe"
-        avatar="/images/user1.jpg"
-        image="/images/post1.jpg"
-        likes={120}
-        caption="Enjoying the sunny vibes!"
-      />
-      <Post
-        username="emma_w"
-        avatar="/images/user2.jpg"
-        image="/images/post2.jpg"
-        likes={340}
-        caption="Coffee time ☕"
-      />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
-}
+};
+
+export default Home;
