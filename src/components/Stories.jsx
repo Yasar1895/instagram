7e052c3176b778f96ba5feeeb48bd379346d36bc
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-16 22:34:49 +0530

    Update Stories.jsx

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,18 +1,26 @@
-export default function Stories() {
+import React from "react";
+import "./Stories.css";
+
+const Stories = () => {
   const stories = [
-    { id: 1, username: "john_doe", image: "/images/user1.jpg" },
-    { id: 2, username: "emma_w", image: "/images/user2.jpg" },
-    { id: 3, username: "alex99", image: "/images/user3.jpg" },
+    { id: 1, username: "you", img: "/images/story1.jpg" },
+    { id: 2, username: "alex", img: "/images/story2.jpg" },
+    { id: 3, username: "maria", img: "/images/story3.jpg" },
+    { id: 4, username: "john", img: "/images/story4.jpg" },
   ];
 
   return (
     <div className="stories">
-      {stories.map(story => (
+      {stories.map((story) => (
         <div key={story.id} className="story">
-          <img src={story.image} alt={story.username} className="story-avatar" />
+          <div className="story-ring">
+            <img src={story.img} alt={story.username} />
+          </div>
           <p>{story.username}</p>
         </div>
       ))}
     </div>
   );
-}
+};
+
+export default Stories;
