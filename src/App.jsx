b2ae9ac56203034cb455d3b8c01530d3d8ed5972
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-16 21:21:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,22 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import BottomNav from "./components/BottomNav";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
-import Profile from "./pages/Profile";
-import PostPage from "./pages/PostPage";
 import Messages from "./pages/Messages";
-import PrivateRoute from "./components/PrivateRoute";
-import { useAuth } from "./contexts/AuthContext";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  const { user } = useAuth();
-
   return (
-    <div className="app-root">
-      {user && <Navbar />}
+    <Router>
+      <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/explore"
-          element={
-            <PrivateRoute>
-              <Explore />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/profile/:id"
-          element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/post/:id"
-          element={
-            <PrivateRoute>
-              <PostPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/messages"
-          element={
-            <PrivateRoute>
-              <Messages />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-        <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
-        <Route path="*" element={<div style={{ padding: 24 }}>Not found</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
-    </div>
+      <BottomNav />
+    </Router>
   );
 }
