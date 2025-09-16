2450ea4214f6a71abfdabe62abba0edffe427628
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-16 22:39:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import BottomNav from "./components/BottomNav";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
-import Messages from "./pages/Messages";
 import Profile from "./pages/Profile";
+import Messages from "./pages/Messages";
+import PostPage from "./pages/PostPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PrivateRoute from "./components/PrivateRoute";
 
-export default function App() {
+const App = () => {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+
+      {/* Main content */}
+      <div style={{ paddingBottom: "60px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* Protected route example */}
+          <Route
+            path="/create"
+            element={
+              <PrivateRoute>
+                <PostPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </div>
+
       <BottomNav />
     </Router>
   );
-}
+};
+
+export default App;
